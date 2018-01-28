69dab3d36c8df829df96034c6e7439dc333a0ee3
Tinny Nyasha <lankienyashanu@gmail.com>
2018-01-29 00:59:53 +0200

    ../views/
    ../views/

--- controllers/EntryController.js
+++ controllers/EntryController.js
@@ -57,7 +57,7 @@ entryController.blank = function(req, res) {
       console.log("Error:", err);
     }
     else {
-      res.render("../views/Entry/blank", {entry: entry});
+      res.render("Entry/blank", {entry: entry});
     }
   });
 };
